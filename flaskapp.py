03b72df7eab921e168c869bd11051b046b3c3579
vaibhav <vaibhavkholase41@gmail.com>
2018-10-31 07:12:06 +0000

    my python code

---
+++ flaskapp.py
@@ -0,0 +1,15 @@
+from flask import Flask, flash, redirect, render_template, request, session, abort
+ 
+app = Flask(__name__)
+ 
+@app.route("/")
+def index():
+#    return render_template('test.html')
+     return "Flask App!"
+ 
+@app.route("/hello/")
+def hello():
+    return render_template('test.html')
+ 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)

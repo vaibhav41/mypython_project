9960e33de94051b7854953e3840e0712772950e1
vaibhav41 <vaibhavkholase41@gmail.com>
2018-11-05 09:07:58 +0000

    resolved confilct

--- flaskapp.py
+++ flaskapp.py
@@ -1,15 +1,17 @@
-from flask import Flask, redirect, render_template
+from flask import Flask,redirect, render_template
+##from flask import Flask, flash, redirect, render_template, request, session, abort
  
 app = Flask(__name__)
  
 @app.route("/")
 def index():
 #    return render_template('test.html')
-     return "Congrtatulation you have successfully deployed Flask App!"
+     return "congratulation you have successfully deploued Flask Web  App!"
  
 @app.route("/hello/")
 def hello():
-    return render_template('test.html')
+    return render_template('index.html')
+    #return render_template('test.html')
  
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
